fd60d5b648df30432528d5ff8bad34cb879043be
Cristian Ayala <cdap_13@hotmail.com>
2021-09-06 22:48:09 -0500

    se validan inputs y finaliza funcionalidad de agregar a inventario

--- js/practica.js
+++ js/practica.js
@@ -4,95 +4,114 @@ class Electrodomestico{
     constructor(consumo, esNacional, cantidad){
         this.consumo=consumo;
         this.esNacional=esNacional;
-        this.cantidad = cantidad;
-        this.valor = calcularValor(consumo,esNacional);
-    }    
+        this.cantidad = cantidad;        
+    } 
+    
+    get valor(){
+        return this.calcularValor(this.consumo,this.esNacional);
+    }
+
+    
+    calcularValor(consumo,esNacional){
+        let valorConsumo = 0;
+        let valorProcedencia = 0;
+        if(esNacional){valorProcedencia=250000;}else{valorProcedencia=350000}
+        switch(consumo){        
+            case 'A':
+                valorConsumo = 450000;
+                break;
+            case 'B':
+                valorConsumo = 350000;
+                break;
+            case 'C':
+                valorConsumo = 250000;
+                break;
+            default:
+                break;
+        }
+        return (valorConsumo+valorProcedencia);
+    }
 }
 
-function calcularValor(consumo,esNacional){
-    let valorConsumo = 0;
-    let valorProcedencia = 0;
-    if(esNacional){valorProcedencia=250000;}else{valorProcedencia=350000}
-    switch(consumo){        
-        case 'A':
-            valorConsumo = 450000;
-            break;
-        case 'B':
-            valorConsumo = 350000;
-            break;
-        case 'C':
-            valorConsumo = 250000;
-            break;
-        default:
-            break;
-    }
-    return (valorConsumo+valorProcedencia);
-}
+
 
 class Televisor extends Electrodomestico{
     constructor(consumo, esNacional,cantidad, tamanio,esSintonizador){
         super(consumo,esNacional,cantidad);
         this.tamanio=tamanio;
         this.esSintonizador=esSintonizador;
-        this.valor = calcularValorTV(consumo,esNacional,tamanio,esSintonizador);
+    }
+
+    get valor(){
+        return this.calcularValorTV(this.consumo,this.esNacional,this.tamanio,this.esSintonizador);
+    }
+
+    calcularValorTV(consumo,esNacional, tamanio, esSintonizador){
+        let valorConsumo = 0;
+        let valorProcedencia = 0;
+        let precio = 0;
+        if(esNacional){valorProcedencia=250000;}else{valorProcedencia=350000}
+        switch(consumo){        
+            case "A":
+                valorConsumo = 450000;
+                break;
+            case "B":
+                valorConsumo = 350000;
+                break;
+            case "C":
+                valorConsumo = 250000;
+                break;
+            default:
+                break;
+        }
+        precio = (valorConsumo+valorProcedencia);
+        if (tamanio>40){precio = precio*1.3;}
+        if (esSintonizador){precio =precio+250000;}
+        return precio;
     }
 }
 
-function calcularValorTV(consumo,esNacional, tamanio, esSintonizador){
-    let valorConsumo = 0;
-    let valorProcedencia = 0;
-    let precio = 0;
-    if(esNacional){valorProcedencia=250000;}else{valorProcedencia=350000}
-    switch(consumo){        
-        case "A":
-            valorConsumo = 450000;
-            break;
-        case "B":
-            valorConsumo = 350000;
-            break;
-        case "C":
-            valorConsumo = 250000;
-            break;
-        default:
-            break;
-    }
-    precio = (valorConsumo+valorProcedencia);
-    if (tamanio>40){precio = precio*1.3;}
-    if (esSintonizador){precio =precio+250000;}
-    return precio;
-}
+
 
 class Nevera extends Electrodomestico{
     constructor(consumo, esNacional,cantidad, capacidad){
         super(consumo,esNacional,cantidad);
         this.capacidad=capacidad;
-        this.valor = calcularValorNevera(consumo,esNacional, capacidad);
+    }
+
+    get valor(){
+        return this.calcularValorNevera(this.consumo,this.esNacional, this.capacidad);
+    }
+    calcularValorNevera(consumo,esNacional, capacidad){
+        let valorConsumo = 0;
+        let valorProcedencia = 0;
+        let precio = 0;
+        if(esNacional){valorProcedencia=250000;}else{valorProcedencia=350000}
+        switch(consumo){        
+            case "A":
+                valorConsumo = 450000;
+                break;
+            case "B":
+                valorConsumo = 350000;
+                break;
+            case "C":
+                valorConsumo = 250000;
+                break;
+            default:
+                break;
+        }
+        precio = (valorConsumo+valorProcedencia);
+        if (capacidad>120){
+            precio = precio* ((Math.floor((capacidad-120)/10))*0.05+1);
+        }
+        return precio;
     }
 }
 
-function calcularValorNevera(consumo,esNacional, capacidad){
-    let valorConsumo = 0;
-    let valorProcedencia = 0;
-    let precio = 0;
-    if(esNacional){valorProcedencia=250000;}else{valorProcedencia=350000}
-    switch(consumo){        
-        case "A":
-            valorConsumo = 450000;
-            break;
-        case "B":
-            valorConsumo = 350000;
-            break;
-        case "C":
-            valorConsumo = 250000;
-            break;
-        default:
-            break;
+class Inventario{
+    constructor(arrayElectrodomesticos){        
+        this.arrayElectrodomesticos = arrayElectrodomesticos;
     }
-    precio = (valorConsumo+valorProcedencia);
-    if (capacidad>120){
-        precio = precio* ((Math.floor((capacidad-120)/10))*0.05+1);
-    }
-    return precio;
 }
 
     function validarConsumo(consumo){
@@ -114,58 +133,10 @@ function calcularValorNevera(consumo,esNacional, capacidad){
         }
     }
 
-    function validarEsNacional(esNacional){
-        switch(esNacional){
-            case "true": 
-                return true;
-            case "false":
-                return true;
-            case true:
-                return true;
-            case false:
-                return true;  
-            default:
-                return false;
-        }
-    }
-
-    function validarCantidad(cantidad){
+    function validarNumeroPositivo(numero){
         try {
-            parseInt(cantidad);
-            if(cantidad>0){return true;}else{return false;}
-        } catch (error) {
-            return false;
-        }
-    }
-
-    function validarTamanio(tamanio){
-        try {
-            parseInt(tamanio);
-            if(tamanio>0){return true;}else{return false;}
-        } catch (error) {
-            return false;
-        }
-    }
-
-    function validarEsSintonizador(esSintonizador){
-        switch(esSintonizador){
-            case "true": 
-                return true;
-            case "false":
-                return true;
-            case true:
-                return true;
-            case false:
-                return true;  
-            default:
-                return false;
-        }
-    }
-
-    function validarCapacidad(capacidad){
-        try {
-            parseInt(capacidad);
-            if(capacidad>0){return true;}else{return false;}
+            parseInt(numero);
+            if(numero>=0){return true;}else{return false;}
         } catch (error) {
             return false;
         }
@@ -192,22 +163,42 @@ function calcularValorNevera(consumo,esNacional, capacidad){
                 case 1:
                     validadorConsumo = false;
                     while(!validadorConsumo){
-                        consumo = prompt("Consumo ('A','B','C')",'A');
+                        consumo = prompt("Consumo ('A','B','C')","A");
                         validadorConsumo = validarConsumo(consumo);
                     }    
                     validadorEsNacional=false;
                     while(!validadorEsNacional){
-                        esNacional = prompt("esNacional? (true or false)",true);
-                        validadorEsNacional =  validarEsNacional(esNacional);
+                        esNacional = prompt("esNacional? (true or false)","");
+                        if (esNacional.toLowerCase()=== "true"){
+                            esNacional = true;
+                            validadorEsNacional =  true;
+                        }else if(esNacional.toLowerCase()=== "false"){
+                            esNacional = false;
+                            validadorEsNacional =  true;
+                        }                        
                     }
                     validadorCantidad=false;
                     while(!validadorCantidad){
                         cantidad=prompt("cantidad:",1);
-                        validadorCantidad =  validarCantidad(cantidad);
+                        validadorCantidad =  validarNumeroPositivo(cantidad);
                     }
                     var electrodomesticoGeneral = new Electrodomestico(consumo,Boolean(esNacional),Number(cantidad));
                     alert("electrodomesticoGeneral :" +"\nConsumo: " +electrodomesticoGeneral.consumo + "\nEs nacional? :"+electrodomesticoGeneral.esNacional+"\nCantidad: "+electrodomesticoGeneral.cantidad+ "\nValor: " +electrodomesticoGeneral.valor );              
-                    arrayElectrodomesticos.push(electrodomesticoGeneral);
+                    
+                    let elementoEncontrado = false;
+                    for(let i=0;i<arrayElectrodomesticos.length;i++){
+                        let objeto = arrayElectrodomesticos[i] instanceof Electrodomestico;
+                        if(!(arrayElectrodomesticos[i] instanceof Televisor)&& !(arrayElectrodomesticos[i] instanceof Nevera)){
+                            if(arrayElectrodomesticos[i].consumo == electrodomesticoGeneral.consumo && arrayElectrodomesticos[i].esNacional==electrodomesticoGeneral.esNacional){
+                                arrayElectrodomesticos[i].cantidad = arrayElectrodomesticos[i].cantidad + electrodomesticoGeneral.cantidad;
+                                elementoEncontrado = true;
+                            }
+                        }
+                    }
+                    if(!elementoEncontrado){
+                        arrayElectrodomesticos.push(electrodomesticoGeneral);
+                    }
+                    
                     break;
                 case 2:
                     validadorConsumo = false;
@@ -218,28 +209,52 @@ function calcularValorNevera(consumo,esNacional, capacidad){
                     validadorEsNacional=false;
                     while(!validadorEsNacional){
                         esNacional = prompt("esNacional? (true or false)",true);
-                        validadorEsNacional =  validarEsNacional(esNacional);
+                        if (esNacional.toLowerCase()=== "true"){
+                            esNacional = true;
+                            validadorEsNacional =  true;
+                        }else if(esNacional.toLowerCase()=== "false"){
+                            esNacional = false;
+                            validadorEsNacional =  true;
+                        }
                     }
                     validadorCantidad=false;
                     while(!validadorCantidad){
                         cantidad=prompt("cantidad:",1);
-                        validadorCantidad =  validarCantidad(cantidad);
+                        validadorCantidad =  validarNumeroPositivo(cantidad);
                     }
                     validadorTamanio=false;
                     while(!validadorTamanio){
                         tamanio=prompt("tamanio en pulgadas:",1);
-                        validadorTamanio =  validarTamanio(tamanio);
+                        validadorTamanio =  validarNumeroPositivo(tamanio);
                     }
                     validadorEsSintonizador=false;
                     while(!validadorEsSintonizador){
                         esSintonizador = prompt("esSintonizador? (true or false)",true);
-                        validadorEsSintonizador = validarEsSintonizador(esSintonizador);
+                        if (esSintonizador.toLowerCase()=== "true"){
+                            esSintonizador = true;
+                            validadorEsSintonizador =  true;
+                        }else if(esSintonizador.toLowerCase()=== "false"){
+                            esSintonizador = false;
+                            validadorEsSintonizador =  true;
+                        }       
                     }
-                    ///alert(typeof(consumo) + "\n"+typeof(esNacional)+"\n"+typeof(cantidad)+"\n"+typeof(tamanio)+"\n"+typeof(esSintonizador));
-                    var tv = new Televisor(consumo,Boolean(esNacional),Number(cantidad),Number(tamanio),Boolean(esSintonizador));
+                    var tv = new Televisor(consumo,Boolean(esNacional),Number(cantidad),Number(tamanio),Boolean(esSintonizador));                    
                     alert("TV :" +"\nConsumo: " +tv.consumo + "\nEs nacional? :"+tv.esNacional+"\nCantidad: "+tv.cantidad+"\nTamanio: "+ tv.tamanio+"\nEs sintonizador? "+ tv.esSintonizador + "\nValor: " +tv.valor);                              
-                    arrayTelevisores.push(tv);
+                    let tvEncontrado = false;
+                    for(let i=0;i<arrayElectrodomesticos.length;i++){
+                        let objeto = arrayElectrodomesticos[i] instanceof Televisor;
+                        if(objeto==true){
+                            if(arrayElectrodomesticos[i].consumo == tv.consumo && arrayElectrodomesticos[i].esNacional==tv.esNacional&&arrayElectrodomesticos[i].tamanio==tv.tamanio&&arrayElectrodomesticos[i].esSintonizador==tv.esSintonizador){
+                                arrayElectrodomesticos[i].cantidad = arrayElectrodomesticos[i].cantidad + tv.cantidad;
+                                tvEncontrado = true;
+                            }
+                        }
+                    }
+                    if(!tvEncontrado){
+                        arrayElectrodomesticos.push(tv);
+                    }
                     break;
+                    
                 case 3:
                     validadorConsumo = false;
                     while(!validadorConsumo){
@@ -249,21 +264,39 @@ function calcularValorNevera(consumo,esNacional, capacidad){
                     validadorEsNacional=false;
                     while(!validadorEsNacional){
                         esNacional = prompt("esNacional? (true or false)",true);
-                        validadorEsNacional =  validarEsNacional(esNacional);
+                        if (esNacional.toLowerCase()=== "true"){
+                            esNacional = true;
+                            validadorEsNacional =  true;
+                        }else if(esNacional.toLowerCase()=== "false"){
+                            esNacional = false;
+                            validadorEsNacional =  true;
+                        }
                     }
                     validadorCantidad=false;
                     while(!validadorCantidad){
                         cantidad=prompt("cantidad:",1);
-                        validadorCantidad =  validarCantidad(cantidad);
+                        validadorCantidad =  validarNumeroPositivo(cantidad);
                     }
                     validadorCapacidad=false;
                     while(!validadorCapacidad){
                         capacidad=prompt("capacidad:",1);
-                        validadorCapacidad =  validarCapacidad(capacidad);
+                        validadorCapacidad =  validarNumeroPositivo(capacidad);
                     }
                     var nevera = new Nevera(consumo,Boolean(esNacional),Number(cantidad),Number(capacidad));
                     alert("nevera :" +"\nConsumo: " +nevera.consumo + "\nEs nacional? :"+nevera.esNacional+"\nCantidad: "+nevera.cantidad+"\nCapacidad: "+ nevera.capacidad+ "\nValor: " +nevera.valor);              
-                    arrayNeveras.push(nevera);
+                    let neveraEncontrada = false;
+                    for(let i=0;i<arrayElectrodomesticos.length;i++){
+                        let objeto = arrayElectrodomesticos[i] instanceof Nevera;
+                        if(objeto==true){
+                            if(arrayElectrodomesticos[i].consumo == nevera.consumo && arrayElectrodomesticos[i].esNacional==nevera.esNacional && arrayElectrodomesticos[i].capacidad==nevera.capacidad){
+                                arrayElectrodomesticos[i].cantidad = arrayElectrodomesticos[i].cantidad + nevera.cantidad;
+                                neveraEncontrada = true;
+                            }
+                        }
+                    }
+                    if(!neveraEncontrada){
+                        arrayElectrodomesticos.push(nevera);
+                    }
                     break;
                     case 4:
                     break;
@@ -273,25 +306,12 @@ function calcularValorNevera(consumo,esNacional, capacidad){
             }
         }while (parseInt(opt)!==4);
     }
-    
-    
-
-
-
 
 let arrayElectrodomesticos =[];
-let arrayTelevisores =[];
-let arrayNeveras =[];
 
 menuInventario();
-
 arrayElectrodomesticos.forEach(function(elemento, indice, array) {
     console.log(elemento, indice);
 })
-arrayTelevisores.forEach(function(elemento, indice, array) {
-    console.log(elemento, indice);
-})
-arrayNeveras.forEach(function(elemento, indice, array) {
-    console.log(elemento, indice);
-})  
+
 
